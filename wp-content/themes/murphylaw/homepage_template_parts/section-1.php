49d2d1ff115fb33fc75d8fde054e9990ee124101
Garrett Cullen <garrettwcullen@gmail.com>
2018-01-30 09:52:17 -0800

    push

---
+++ wp-content/themes/murphylaw/homepage_template_parts/section-1.php
@@ -0,0 +1,31 @@
+<section id="section_one">
+	
+	<div class="sec_one_inner">
+		
+		<span class="large_header">Don't Be A Victim Twice</span><!-- large_header -->
+		
+		<a class="consultation_button" href="">Free Case Consultation</a><!-- consultation_button -->
+		
+		<div class="sec_one_sp_wrapper">
+			
+			<div class="sp_box_one">
+				
+				<span class="sp_header">We Will Fight For You</span><!-- sp_header -->
+				
+				<p>When facing Murphy Law Firm, LLC insurance companies know they have to make a fair offer that fully compensates you for all damages, or they will have to fight a battle-tested trial lawyer.</p>
+				
+			</div><!-- sp_box_one -->
+			
+			<div class="sp_box_two sp_box">
+				
+				<span class="sp_small_title">record results</span><!-- sp_small_title -->
+				
+			</div><!-- sp_box_two -->
+			
+			<div class="sp_box_three sp_box"></div><!-- sp_box_three -->
+			
+		</div><!-- sec_one_sp_wrapper -->
+		
+	</div><!-- sec_one_inner -->
+	
+</section><!-- section_one -->
