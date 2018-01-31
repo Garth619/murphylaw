44da5cf179e0b162e6dd05307efb1676097093ad
Garrett Cullen <garrettwcullen@gmail.com>
2018-01-31 09:54:31 -0800

    push

---
+++ wp-content/themes/murphylaw/homepage_template_parts/section-2.php
@@ -0,0 +1,88 @@
+<section id="section_two">
+	
+	<div class="sec_two_inner">
+		
+		
+		<div class="intro_wrapper">
+			
+			<img class="baton_rouge_svg" src="<?php bloginfo('template_directory');?>/images/icon-spear.svg"/>
+			
+			<div class="intro_inner_wrapper">
+			
+			<h1>
+				
+				<span class="large_header">Baton <span class="lower_large_header">Rouge</span></span>
+				
+				<span class="small_header">Personal Injury Attorneys</span>
+			
+			</h1>
+			
+			<div class="intro_inner_content">
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id quam at nisi venenatis tempus eget vel orci. Nam bibendum luctus neque id euismod. Etiam a ultricies felis. Integer malesuada varius justo a eget fermentum. Mauris vel tristique mi. Aenean libero tortor, condimentum eget efficitur sed, pulvinar dictum elit.Bibendum luctus neque id etaim.</p>
+				
+				<a class="download_button" href="">Download Free E-Book</a><!-- download_button -->
+				
+			</div><!-- intro_inner_content -->
+			
+			
+			</div><!-- intro_inner_wrapper -->
+			
+			
+			
+		</div><!-- intro_wrapper -->
+		
+		
+		<div class="sec_two_content content">
+			
+			
+			<div class="col">
+				
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+
+				<p>Pellentesque sodales fermentum ante eget pretium. Cras quis ornare libero, quis laoreet ante. Mauris posuere, velit eget condimentum sodales, urna magna tincidunt tellus, vel imperdiet orci enim a lacus. Fusce massa magna, porta eget nisl ut, auctor suscipit augue. In ullamcorper tempor velit in mattis. Nullam cursus sollicitudin sem, a cursus magna porta ac. In a justo tristique, auctor leo nec, gravida orci. Quisque massa nisi, imperdiet a imperdiet non, rutrum in justo. Cras tempus sollicitudin ipsum, et mattis augue porttitor et. Praesent malesuada est sed odio egestas eleifend. Phasellus tortor libero, imperdiet et neque quis, varius volutpat dolor. Nam felis ligula, condimentum quis sem at, fermentum scelerisque felis.</p>
+				
+				<blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id quam at nisi venenatis</blockquote>
+
+
+				<p>Sed suscipit pretium sodales. Pellentesque venenatis neque turpis, ac porttitor ex tristique eu. Curabitur laoreet arcu quis ultricies sollicitudin. Maecenas in est ex. Pellentesque sem felis, convallis in massa id, posuere rhoncus tellus. Maecenas tempor convallis diam et ultrices. Praesent gravida convallis nisi eget pharetra.
+Etiam in nibh mollis, suscipit elit quis, elementum diam. Quisque varius faucibus neque, ac maximus leo dictum quis. Aliquam sagittis ante mi, quis semper ante condimentum id. Vivamus malesuada eget ante vitae auctor. In ut orci ante. Quisque nulla ligula, congue eu accumsan quis, aliquet at orci. Maecenas varius urna et metus ultrices, bibendum malesuada odio venenatis. Vivamus scelerisque dignissim magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. mentum id. Vivamus malesuada eget ante vitae auctor. In ut orci ante. Quisque nulla ligula, congue eu accumsan quis, aliquet at orci. Maecenas varius urna et metus ultrices, bibendum malesuada odio venenatis. Vivamus scelerisque dignissim magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+			
+			</div><!-- col -->
+
+			<div class="col">
+				
+				<p>Quisque non egestas justo, ac pellentesque dolor. In et diam sagittis, semper lorem id, blandit velit. Integer mattis metus vel nisi commodo porttitor. Aenean commodo commodo facilisis. Nam arcu erat, lacinia sed lacinia quis, pretium eget enim.</p>
+				
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+				
+				<p>Etiam in nibh mollis, suscipit elit quis, elementum diam. Quisque varius faucibus neque, ac maximus leo dictum quis. Aliquam sagittis ante mi, quis semper ante condimentum id. Vivamus malesuada eget ante vitae auctor. In ut orci ante. Quisque nulla ligula, congue eu accumsan quis, aliquet at orci. Maecenas varius urna et metus ultrices, bibendum malesuada odio venenatis. Vivamus scelerisque dignissim magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Quisque non egestas justo, ac pellentesque dolor.</p>
+				
+			</div><!-- col -->
+			
+			<div class="col">
+				
+				<p>Melentesque venenatis neque turpis, ac porttitor ex tristique eu. Curabitur laoreet arcu quis ultricies sollicitudin. Maecenas in est ex. Pellentesque sem felis, convallis in massa id, posuere rhoncus tellus. Maecenas tempor convallis diam et ultrices. Praesent gravida convallis nisi eget pharetra.</p>
+				
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+
+				<p>Etiam in nibh mollis, suscipit elit quis, elementum diam. Quisque varius faucibus neque, ac maximus leo dictum quis. Aliquam sagittis ante mi, quis semper ante condimentum id. Vivamus malesuada eget ante vitae auctor. In ut orci ante. Quisque nulla ligula, congue eu accumsan quis, aliquet at orci. Maecenas varius urna et metus ultrices, bibendum malesuada odio venenatis. Vivamus scelerisque dignissim magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+				
+				<p>Quisque non egestas justo, ac pellentesque dolor. In et diam sagittis, semper lorem id, blandit velit. Integer mattis metus vel nisi commodo porttitor. Aenean commodo commodo facilisis. Nam arcu erat, lacinia sed lacinia quis, pretium eget enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed maximus quam vestibulum imperdiet interdum. Cras rhoncus arcu ornare sapien sollicitudin pulvinar. Vivamus placerat purus sit amet sapien malesuada feugiat. Ut accumsan finibus nunc.. Cras ultricies turpis nec quam egestas faucibus. Integer varius aliquam diam, a rhoncus sem elementum id. Aliquam vel lectus eget risus pulvinar efficitur elementum.</p>
+				
+				
+			</div><!-- col -->
+				
+			</div><!-- sec_two_content -->
+			
+			
+			
+			
+			
+		</div><!-- sec_two_content -->
+		
+		
+	</div><!-- sec_two_inner -->
+	
+	
+</section><!-- section_two -->
