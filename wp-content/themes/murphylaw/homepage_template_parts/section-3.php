807e9ef00c04cd43bf867e6e5023434599c30c76
Garrett Cullen <garrettwcullen@gmail.com>
2018-02-01 14:00:39 -0800

    push

---
+++ wp-content/themes/murphylaw/homepage_template_parts/section-3.php
@@ -0,0 +1,155 @@
+<section id="section_three">
+	
+	<div class="sec_three_box hover_box">
+		
+		<a class="" href="">
+		
+			<div class="hover_inner">
+			
+				<div class="flex_center">
+			
+					<span class="amount">$3.8M</span><!-- amount -->
+			
+					<span class="case_type">18-wheeler Accident</span><!-- case_type -->
+			
+					<?php echo file_get_contents("wp-content/themes/murphylaw/images/new-saints.svg"); ?>
+			
+				</div><!-- flex_center -->
+			
+			</div><!-- hover_inner -->
+		
+			<div class="hover_read_more">
+			
+				<span class="read_more_title">Read More</span><!-- read_more_title -->
+			
+				<div class="red_dots_wrapper">
+				
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+				
+				</div><!-- red_dots -->
+			
+			</div><!-- hover_read_more -->
+		
+			<div class="hover_excerpt">
+			
+				<span class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </span>
+			
+			
+			</div><!-- hover_excerpt -->
+		
+		</a>
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box pic_box" style="background: url(http://murphylaw.com/wp-content/themes/murphylaw/images/results-image-01-desktop.jpg) top center no-repeat;background-size:cover">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box hover_box">
+		
+		
+		<a class="" href="">
+		
+			<div class="hover_inner">
+			
+				<div class="flex_center">
+			
+					<span class="amount">$2.26M</span><!-- amount -->
+			
+					<span class="case_type">Truck Accident</span><!-- case_type -->
+			
+					<?php echo file_get_contents("wp-content/themes/murphylaw/images/new-saints.svg"); ?>
+			
+				</div><!-- flex_center -->
+			
+			</div><!-- hover_inner -->
+		
+			<div class="hover_read_more">
+			
+				<span class="read_more_title">Read More</span><!-- read_more_title -->
+			
+				<div class="red_dots_wrapper">
+				
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+				
+				</div><!-- red_dots -->
+			
+			</div><!-- hover_read_more -->
+		
+			<div class="hover_excerpt">
+			
+				<span class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </span>
+			
+			
+			</div><!-- hover_excerpt -->
+		
+		</a>
+		
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box pic_box" style="background: url(http://murphylaw.com/wp-content/themes/murphylaw/images/results-image-02-desktop.jpg) top center no-repeat;background-size:cover">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box hover_box">
+		
+		
+		<a class="" href="">
+		
+			<div class="hover_inner">
+			
+				<div class="flex_center">
+			
+					<span class="amount">$2M</span><!-- amount -->
+			
+					<span class="case_type">Wrongful Death</span><!-- case_type -->
+			
+					<?php echo file_get_contents("wp-content/themes/murphylaw/images/new-saints.svg"); ?>
+			
+				</div><!-- flex_center -->
+			
+			</div><!-- hover_inner -->
+		
+			<div class="hover_read_more">
+			
+				<span class="read_more_title">Read More</span><!-- read_more_title -->
+			
+				<div class="red_dots_wrapper">
+				
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+					<span class="red_dots"></span>
+				
+				</div><!-- red_dots -->
+			
+			</div><!-- hover_read_more -->
+		
+			<div class="hover_excerpt">
+			
+				<span class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </span>
+			
+			
+			</div><!-- hover_excerpt -->
+		
+		</a>
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box pic_box" style="background: url(http://murphylaw.com/wp-content/themes/murphylaw/images/results-image-03-desktop.jpg) top center no-repeat;background-size:cover">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	
+	
+</section><!-- section_three -->
