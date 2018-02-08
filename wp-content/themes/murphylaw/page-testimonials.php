968878313963c7799e333720ddae4276d8bdf676
Garrett Cullen <garrettwcullen@gmail.com>
2018-02-08 11:04:40 -0800

    push

---
+++ wp-content/themes/murphylaw/page-testimonials.php
@@ -0,0 +1,152 @@
+<?php 
+	
+	
+	/* Template Name: Testimonials */
+	
+	
+	get_header(); ?>
+
+
+<?php get_template_part( 'banner', 'internal' );?>
+
+<div class="main">
+	
+	<div class="container">
+	
+		<h1 class="page_title"><?php the_title();?></h1><!-- page_title -->
+		
+		<div class="testimonials_page_wrapper">
+			
+			
+			<div class="single_testi">
+				
+				
+				<div class="rating_wrapper">
+					
+					<span class="rating">
+					
+						<span class="bold_rating">4.7</span><!-- bold_rating -->
+					
+					 / 5</span><!-- rating -->
+					
+				</div><!-- rating_wrapper -->
+				
+				<span class="testi_type">google reviews</span><!-- testi_type -->
+				
+				
+				<span class="testi_description">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!”</span><!-- testi_description -->
+				
+				
+				<span class="testi_name">Terrell M.</span><!-- testi_name -->
+				
+				
+			</div><!-- single_testi -->
+			
+			<div class="single_testi">
+				
+				
+				<div class="rating_wrapper">
+					
+					<span class="rating">
+					
+						<span class="bold_rating">4.7</span><!-- bold_rating -->
+					
+					 / 5</span><!-- rating -->
+					
+				</div><!-- rating_wrapper -->
+				
+				<span class="testi_type">google reviews</span><!-- testi_type -->
+				
+				
+				<span class="testi_description">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!”</span><!-- testi_description -->
+				
+				
+				<span class="testi_name">Terrell M.</span><!-- testi_name -->
+				
+				
+			</div><!-- single_testi -->
+			
+			<div class="single_testi">
+				
+				
+				<div class="rating_wrapper">
+					
+					<span class="rating">
+					
+						<span class="bold_rating">4.7</span><!-- bold_rating -->
+					
+					 / 5</span><!-- rating -->
+					
+				</div><!-- rating_wrapper -->
+				
+				<span class="testi_type">google reviews</span><!-- testi_type -->
+				
+				
+				<span class="testi_description">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!”</span><!-- testi_description -->
+				
+				
+				<span class="testi_name">Terrell M.</span><!-- testi_name -->
+				
+				
+			</div><!-- single_testi -->
+			
+			<div class="single_testi">
+				
+				
+				<div class="rating_wrapper">
+					
+					<span class="rating">
+					
+						<span class="bold_rating">4.7</span><!-- bold_rating -->
+					
+					 / 5</span><!-- rating -->
+					
+				</div><!-- rating_wrapper -->
+				
+				<span class="testi_type">google reviews</span><!-- testi_type -->
+				
+				
+				<span class="testi_description">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!”</span><!-- testi_description -->
+				
+				
+				<span class="testi_name">Terrell M.</span><!-- testi_name -->
+				
+				
+			</div><!-- single_testi -->
+			
+			<div class="single_testi">
+				
+				
+				<div class="rating_wrapper">
+					
+					<span class="rating">
+					
+						<span class="bold_rating">4.7</span><!-- bold_rating -->
+					
+					 / 5</span><!-- rating -->
+					
+				</div><!-- rating_wrapper -->
+				
+				<span class="testi_type">google reviews</span><!-- testi_type -->
+				
+				
+				<span class="testi_description">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!”</span><!-- testi_description -->
+				
+				
+				<span class="testi_name">Terrell M.</span><!-- testi_name -->
+				
+				
+			</div><!-- single_testi -->
+			
+			
+			
+		</div><!-- testimonials_wrapper -->
+		
+	</div><!-- container -->
+		
+		
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
