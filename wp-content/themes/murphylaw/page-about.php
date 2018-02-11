cd8cd983bac1d3fcade840c45a30b63d08a8c7c8
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-11 11:55:26 -0800

    push

---
+++ wp-content/themes/murphylaw/page-about.php
@@ -0,0 +1,153 @@
+<?php 
+	
+	
+	/* Template Name: About */
+	
+	
+	get_header(); ?>
+
+
+<div class="main">
+	
+<div class="profile_top_wrapper">
+	
+	<div class="profile_inner">
+		
+		<div class="profile_header_wrapper">
+	
+			<h1 class="bio_header">
+		
+				<span class="upper_header">About</span><!-- upper_header -->
+				<span class="lower_header">The Firm</span><!-- lower_header -->
+		
+			</h1><!-- bio_header -->
+		
+<!-- 			<span class="bio_position">Founder / Attorney</span> --><!-- bio_position -->
+		
+		</div><!-- profile_header_wrapper -->
+		
+		<div class="profile_image_wrapper">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/int-about-image-desktop.jpg"/>
+			
+		</div><!-- profile_image_wrapper -->
+		
+	
+	</div><!-- profile_inner -->
+	
+	
+</div><!-- profile_top_wrapper -->
+
+
+<div class="profile_container content">
+	
+	
+	<div class="profile_content">
+		
+		<h2>Curabitur quis egestas purus, suspendisse sodales risus</h2>
+		
+		<p>Integer sed commodo lacus. Suspendisse sit amet egestas metus, vel fringilla eros. Suspendisse semper ante non metus suscipit, aliquam mattis lectus pretium. Praesent tellus lorem, semper sed viverra eu, gravida in elit. In interdum purus ac ornare porta. Etiam eros augue, pretium vel facilisis at, ultricies at eros. Etiam pellentesque, ipsum ut dapibus porttitor, ante velit consectetur sapien, at maximus urna purus sit amet felis. Curabitur et efficitur purus. Integer posuere augue vel leo tincidunt dictum. Mauris vel quam et arcu commodo fringilla. Fusce ultrices ac lacus quis tempor. Maecenas sagittis, nunc quis congue pharetra, mi velit bibendum quam, sit amet malesuada velit sapien vitae velit. Nam ut pretium mi.</p>
+		
+		<p>Pellentesque dignissim facilisis cursus. Nunc eget nunc neque. Nulla purus nibh, placerat id lectus quis, facilisis volutpat dui. Proin vel nibh vitae ligula commodo suscipit at eget massa. Aliquam bibendum nibh eu magna luctus, hendrerit tincidunt libero tristique. Etiam viverra eros eros, ac tempor felis vulputate a.</p>
+		
+		
+		<p>Ut bibendum dui nec erat auctor, vel auctor lorem sodales. Morbi feugiat volutpat massa, non faucibus lectus maximus eu. Phasellus rhoncus vel nulla ultricies lacinia. Phasellus sem ante, rutrum efficitur tortor vel, convallis malesuada erat. Curabitur vitae lacus ornare, dapibus lacus et, lacinia orci. Vivamus interdum pulvinar ipsum non sollicitudin.
+Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu sollicitudin ligula arcu in sem. Integer ullamcorper in augue sed sollicitudin.</p>
+
+
+	<blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id quam at nisi venenatis</blockquote>
+
+	<p>Nam mi tortor, scelerisque nec risus eget, elementum varius tortor. Suspendisse lorem sapien, sollicitudin a velit id, rutrum lobortis velit. Phasellus ut augue a quam fermentum tincidunt. Sed at vehicula tortor.</p>
+	
+	
+	<div class="learn_more_play_wrapper">
+		
+		<div class="play_hack wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">
+			
+			
+		</div><!-- play_hack -->
+		
+		<div class="about_play_button">
+										
+			<div class="about_play_button_inner">
+										
+				<span class="about_tri"></span><!-- about_tri -->
+								
+			</div><!-- about_play_button_inner -->
+							
+			<span class="about_gold_square"></span><!-- white_square -->
+										
+		</div><!-- about_play_button -->
+	
+		<div class="learn_play_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Learn about the firm</div><!-- learn_play_title -->
+		
+	</div><!-- learn_more_play_wrapper -->
+	
+	
+	<p>Donec dapibus lacus eget sapien molestie pellentesque. Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu sollicitudin ligula arcu in sem. Integer ullamcorper in augue sed sollicitudin. Mauris varius ante nibh, sed luctus est laoreet quis. Eget non blandit libero commodo non. Phasellus risus purus, interdum vel mollis non, sagittis viverra ante.</p>
+
+<h2>Curabitur quis egestas purus, suspendisse sodales risus</h2>
+
+<p>Phasellus et magna id ipsum placerat mollis nec sed nunc. Donec id urna ac mi commodo ullamcorper nec eu massa. Sed sed malesuada metus, eu malesuada elit. Nullam pretium, eros vel malesuada rhoncus, ligula orci consectetur lacus, ut efficitur metus diam sed ipsum. Sed sit amet mi malesuada, sodales ipsum vel, pretium metus. </p>
+
+		
+	</div><!-- profile_content -->
+	
+		
+</div><!-- profile_container -->
+		
+		
+		<div class="sec_two_slideshow">
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-01.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-02.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-03.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-04.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-05.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-06.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				<div class="sec_two_single_slide">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/awards-07.png"/>
+					
+				</div><!-- sec_two_single_slide -->
+				
+				
+			</div><!-- sec_two_slideshow -->
+		
+		
+				
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
