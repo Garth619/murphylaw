9fda789e7288d8fc6c8b503b1c4b04d481e063d3
Garrett Cullen <garrettwcullen@gmail.com>
2018-02-14 14:10:36 -0800

    push

--- wp-content/themes/murphylaw/page-about.php
+++ wp-content/themes/murphylaw/page-about.php
@@ -18,8 +18,8 @@
 	
 			<h1 class="bio_header">
 		
-				<span class="upper_header">About</span><!-- upper_header -->
-				<span class="lower_header">The Firm</span><!-- lower_header -->
+				<span class="upper_header"><?php the_field( 'top_line' ); ?></span><!-- upper_header -->
+				<span class="lower_header"><?php the_field( 'bottom_line' ); ?></span><!-- lower_header -->
 		
 			</h1><!-- bio_header -->
 		
@@ -29,7 +29,14 @@
 		
 		<div class="profile_image_wrapper">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/int-about-image-desktop.jpg"/>
+			
+			<?php $about_image = get_field( 'about_image' ); ?>
+			
+			<?php if ( $about_image ) { ?>
+	
+				<img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" />
+
+			<?php } ?>
 			
 		</div><!-- profile_image_wrapper -->
 		
@@ -45,6 +52,9 @@
 	
 	<div class="profile_content">
 		
+		<?php get_template_part( 'loop', 'page' ) ?>
+		
+<!--
 		<h2>Curabitur quis egestas purus, suspendisse sodales risus</h2>
 		
 		<p>Integer sed commodo lacus. Suspendisse sit amet egestas metus, vel fringilla eros. Suspendisse semper ante non metus suscipit, aliquam mattis lectus pretium. Praesent tellus lorem, semper sed viverra eu, gravida in elit. In interdum purus ac ornare porta. Etiam eros augue, pretium vel facilisis at, ultricies at eros. Etiam pellentesque, ipsum ut dapibus porttitor, ante velit consectetur sapien, at maximus urna purus sit amet felis. Curabitur et efficitur purus. Integer posuere augue vel leo tincidunt dictum. Mauris vel quam et arcu commodo fringilla. Fusce ultrices ac lacus quis tempor. Maecenas sagittis, nunc quis congue pharetra, mi velit bibendum quam, sit amet malesuada velit sapien vitae velit. Nam ut pretium mi.</p>
@@ -66,23 +76,23 @@ Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu soll
 		<div class="play_hack wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">
 			
 			
-		</div><!-- play_hack -->
+		</div>
 		
 		<div class="about_play_button">
 										
 			<div class="about_play_button_inner">
 										
-				<span class="about_tri"></span><!-- about_tri -->
+				<span class="about_tri"></span>
 								
-			</div><!-- about_play_button_inner -->
+			</div>
 							
-			<span class="about_gold_square"></span><!-- white_square -->
+			<span class="about_gold_square"></span>
 										
-		</div><!-- about_play_button -->
+		</div>
 	
-		<div class="learn_play_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Learn about the firm</div><!-- learn_play_title -->
+		<div class="learn_play_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Learn about the firm</div>
 		
-	</div><!-- learn_more_play_wrapper -->
+	</div>
 	
 	
 	<p>Donec dapibus lacus eget sapien molestie pellentesque. Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu sollicitudin ligula arcu in sem. Integer ullamcorper in augue sed sollicitudin. Mauris varius ante nibh, sed luctus est laoreet quis. Eget non blandit libero commodo non. Phasellus risus purus, interdum vel mollis non, sagittis viverra ante.</p>
@@ -90,6 +100,7 @@ Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu soll
 <h2>Curabitur quis egestas purus, suspendisse sodales risus</h2>
 
 <p>Phasellus et magna id ipsum placerat mollis nec sed nunc. Donec id urna ac mi commodo ullamcorper nec eu massa. Sed sed malesuada metus, eu malesuada elit. Nullam pretium, eros vel malesuada rhoncus, ligula orci consectetur lacus, ut efficitur metus diam sed ipsum. Sed sit amet mi malesuada, sodales ipsum vel, pretium metus. </p>
+-->
 
 		
 	</div><!-- profile_content -->
@@ -99,48 +110,32 @@ Maecenas placerat, lorem at volutpat viverra, libero enim commodo augue, eu soll
 		
 		
 		<div id="about_slideshow" class="sec_two_slideshow">
+			
+			
+			
+			<?php if(get_field('about_logos')): ?>
+			 
+				<?php while(has_sub_field('about_logos')): ?>
+			 
+					<div class="sec_two_single_slide">
+					
+						<?php $logo = get_sub_field( 'logo' ); ?>
+		
+						<?php if ( $logo ) { ?>
+			
+							<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
+		
+						<?php } ?>
+					
+					</div><!-- sec_two_single_slide -->
+			    
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
+			
+			
 				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-01.png"/>
-					
-				</div><!-- sec_two_single_slide -->
 				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-02.png"/>
-					
-				</div><!-- sec_two_single_slide -->
-				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-03.png"/>
-					
-				</div><!-- sec_two_single_slide -->
-				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-04.png"/>
-					
-				</div><!-- sec_two_single_slide -->
-				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-05.png"/>
-					
-				</div><!-- sec_two_single_slide -->
-				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-06.png"/>
-					
-				</div><!-- sec_two_single_slide -->
-				
-				<div class="sec_two_single_slide">
-					
-					<img src="<?php bloginfo('template_directory');?>/images/awards-07.png"/>
-					
-				</div><!-- sec_two_single_slide -->
 				
 				
 			</div><!-- sec_two_slideshow -->
