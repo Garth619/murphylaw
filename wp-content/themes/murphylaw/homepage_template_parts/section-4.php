9a9adaac93f793723a8c5f7cfa5a5b8c46af6f9d
Garrett Cullen <garrettwcullen@gmail.com>
2018-02-16 15:29:16 -0800

    push

--- wp-content/themes/murphylaw/homepage_template_parts/section-4.php
+++ wp-content/themes/murphylaw/homepage_template_parts/section-4.php
@@ -24,80 +24,40 @@
 	<div class="sec_four_slideshow">
 		
 		
-		<div class="sec_four_slide">
+			<?php if(get_field('testimonials_page', 16)): ?>
+		 
+			<?php while(has_sub_field('testimonials_page',16)): ?>
+		 
 			
-			<div class="ratings_wrapper">
+					<div class="sec_four_slide">
+			
+						<div class="ratings_wrapper">
 				
-				<span class="ratings_title">
+							<span class="ratings_title">
 				
-					<span class="bold">4.7</span>
+								<span class="bold"><?php the_sub_field( 'review_rating' ); ?></span>
 					
-					/ 5
+								/ 5
 					
-				</span><!-- ratings_title -->
+							</span><!-- ratings_title -->
 				
-				<span class="review_source">Google Reviews</span><!-- review_source -->
+							<span class="review_source"><?php the_sub_field( 'review_type' ); ?></span><!-- review_source -->
 				
-			</div><!-- ratings_wrapper -->
+						</div><!-- ratings_wrapper -->
 			
-			<span class="slide_content">My experience with Murphy Law Firm was an overall success!  Peyton himself called to check on me periodically through the entire process.  I was extremely happy with the outcome. Thanks Peyton!!</span><!-- slide_content -->
+						<span class="slide_content"><?php the_sub_field( 'review_description' ); ?></span><!-- slide_content -->
 			
 			
-			<span class="name">Terrell M.</span>
+						<span class="name"><?php the_sub_field( 'review_name' ); ?></span>
 			
 		</div><!-- sec_four_slide -->
+		    
+			<?php endwhile; ?>
+		 
+		<?php endif; ?>
 		
-		<div class="sec_four_slide">
-			
-			<div class="ratings_wrapper">
-				
-				<span class="ratings_title">
-				
-					<span class="bold">4.8</span>
-					
-					/ 5
-					
-				</span><!-- ratings_title -->
-				
-				<span class="review_source">Lorem Ipsum</span><!-- review_source -->
-				
-			</div><!-- ratings_wrapper -->
-			
-			<span class="slide_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris!</span><!-- slide_content -->
-			
-			
-			<span class="name">Name 2</span>
-			
-		</div><!-- sec_four_slide -->
-		
-		<div class="sec_four_slide">
-			
-			<div class="ratings_wrapper">
-				
-				<span class="ratings_title">
-				
-					<span class="bold">4.9</span>
-					
-					/ 5
-					
-				</span><!-- ratings_title -->
-				
-				<span class="review_source">Dolor Sit</span><!-- review_source -->
-				
-			</div><!-- ratings_wrapper -->
-			
-			<span class="slide_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostruris!</span><!-- slide_content -->
-			
-			
-			<span class="name">Name 3</span>
-			
-		</div><!-- sec_four_slide -->
-		
-		
-	
-		
-	
-		
+
+
 	</div><!-- sec_four_slideshow -->
 	
 	
