80506b62868f40f10ca6d10c65a40e35d4368a6f
Garrett Cullen <garrettwcullen@gmail.com>
2018-02-19 12:54:32 -0800

    push

---
+++ wp-content/themes/murphylaw/homepage_template_parts/section-2b.php
@@ -0,0 +1,33 @@
+<section id="section_two_b">
+	
+	
+	<div class="sec_two_b_inner">
+		
+		<div class="sec_two_b_left content">
+			
+			<h2>Types of Compensation in Personal Injury Lawsuits</h2>
+			
+			<p>The plaintiff in a personal injury case can seek compensation for all the damages he or she sustained due to the defendant’s negligence. Generally, damages come in two forms: economic, and non-economic. A settlement or case award pays economic damages directly as these are typically measurable monetary amounts. Non-economic damages pertain to more abstract damages, such as damage to reputation, pain and suffering, permanent disability, or loss of enjoyment of life. Most personal injury claims will lead to compensation including medical expenses, pain and suffering, lost income, and property damage.</p>
+			
+			<p>Louisiana state laws impact personal injury lawsuits in many ways, and our firm has the experience necessary to handle these statutes. Unlike most of the rest of the country, Louisiana’s statute of limitations for personal injury claims is only one year, so it’s imperative to act quickly to protect your rights after an injury. Louisiana also follows a pure comparative negligence law, so even if a plaintiff is 99% at fault, Louisiana law will allow the plaintiff to receive the remaining 1%.</p>
+			
+		</div><!-- sec_two_b_left -->
+		
+		<div class="sec_two_b_right">
+			
+			
+			<span class="large_header">Louisiana</span><!-- large_header -->
+			
+			<div class="info_box">
+				
+				Reach out to Murphy Law Firm today to schedule a consultation about your personal injury case. One of our attorneys will review the details of your claim and let you know whether or not a lawsuit would be a good choice. Get in touch with us today to get started. 
+				
+			</div><!-- info_box -->
+			
+			
+		</div><!-- sec_two_b_right -->
+		
+	</div><!-- sec_two_b_inner -->
+	
+	
+</section><!-- section_two_b -->
